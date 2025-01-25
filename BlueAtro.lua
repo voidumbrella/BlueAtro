f291f5d29965db1ec516f51637f8b724249dac4f
voidumbrella <voidumbrella@gmail.com>
2025-01-25 14:37:49 -0500

    Add a new context for joker destruction and an example joker for it

--- BlueAtro.lua
+++ BlueAtro.lua
@@ -22,10 +22,11 @@ SMODS.Joker({
 	config = { extra = { mult = 11 } },
 	rarity = 1,
 	cost = 4,
-	loc_vars = function(self, info_queue, card)
+	blueprint_compat = true,
+	loc_vars = function(_, info_queue, card)
 		return { vars = { card.ability.extra.mult } }
 	end,
-	calculate = function(self, card, context)
+	calculate = function(_, card, context)
 		if context.joker_main then
 			local ok = true
 			for i = 1, #context.scoring_hand do
@@ -62,26 +63,30 @@ SMODS.Joker({
 	config = { extra = { mult = 0, mult_gain = 3 } },
 	rarity = 2,
 	cost = 4,
-	loc_vars = function(self, info_queue, card)
+	loc_vars = function(_, info_queue, card)
 		return { vars = { card.ability.extra.mult, card.ability.extra.mult_gain } }
 	end,
-	calculate = function(self, card, context)
+	calculate = function(_, card, context)
 		if context.joker_main and card.ability.extra.mult > 0 then
 			return {
 				mult_mod = card.ability.extra.mult,
 				message = localize({ type = "variable", key = "a_mult", vars = { card.ability.extra.mult } }),
 			}
-		elseif context.remove_playing_cards and context.scoring_hand and not context.blueprint then
-			local count = #context.removed
-			card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.mult_gain * count
-			-- Below is the "SMOD" way to do it I suppose, but it doesn't work. Bug?
-			-- return {
-			-- 	message = "Upgraded!",
-			-- 	colour = G.C.MULT,
-			-- 	card = card,
-			-- }
-
-			-- See functions/common_events.lua
+		elseif context.remove_playing_cards and not context.blueprint then
+			if not context.removed or #context.removed == 0 then
+				return
+			end
+			card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.mult_gain * #context.removed
+			return {
+				message = localize({
+					type = "variable",
+					key = "a_mult",
+					vars = { card.ability.extra.mult },
+				}),
+				card = card,
+			}
+		elseif context.joker_destroyed and not context.blueprint then
+			card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.mult_gain
 			card_eval_status_text(card, "extra", nil, nil, nil, {
 				message = localize({
 					type = "variable",
